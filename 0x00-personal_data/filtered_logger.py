c02f8a8988ececf4592644eb46d407e62ddcc7e6
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-08-01 20:17:45 +0000

    test

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,71 +1,15 @@
 #!/usr/bin/env python3
-"""Module for handling Personal Data"""
-from typing import List
+""" This script demonstrates the use of regex for replacing occurrences of certain field values in logs. """
 import re
+from typing import List
 import logging
-from os import environ
 import mysql.connector
-
-
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-
-
-def filter_datum(
-    fields: List[str],
-    redaction: str,
-    message: str,
-    separator: str) -> str:
-    """Returns a log message with obfuscated fields"""
-    for field in fields:
-        message = re.sub(f'{field}=.*?{separator}',
-                         f'{field}={redaction}{separator}', message)
-    return message
-
-
-def get_logger() -> logging.Logger:
-    """Returns a Logger Object"""
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
-    logger.addHandler(stream_handler)
-
-    return logger
-
-
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Returns a connector to a MySQL database"""
-    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
-    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = environ.get("PERSONAL_DATA_DB_NAME")
-
-    cnx = mysql.connector.connect(user=username,
-                                  password=password,
-                                  host=host,
-                                  database=db_name)
-    return cnx
-
-
-def main():
-    """Obtain a database connection using get_db and retrieve all rows."""
-    db = get_db()
-    cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM users;")
-    logger = get_logger()
-
-    for row in cursor:
-        message = "; ".join([f"{key}={value}" for key, value in row.items()])
-        logger.info(message)
-
-    cursor.close()
-    db.close()
+import os
 
 
 class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class"""
+    """ Redacting Formatter class for obfuscating PII in log records. """
+
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
@@ -75,10 +19,67 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Filters values in incoming log records using filter_datum"""
-        record.msg = filter_datum(self.fields, self.REDACTION,
-                                  record.getMessage(), self.SEPARATOR)
-        return super(RedactingFormatter, self).format(record)
+        """ Filters sensitive data in log records. """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+PII_FIELDS = ("name", "email", "password", "ssn", "phone")
+
+
+def get_db() -> mysql.connector.connection.MYSQLConnection:
+    """ Establishes a connection to the MySQL database. """
+    connector = mysql.connector.connect(
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME')
+    )
+    return connector
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Obfuscates sensitive data in log messages using regex. """
+    for field in fields:
+        message = re.sub(f'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
+
+
+def get_logger() -> logging.Logger:
+    """ Configures and returns a logger for user data. """
+    log = logging.getLogger("user_data")
+    log.setLevel(logging.INFO)
+    log.propagate = False
+
+    target = logging.StreamHandler()
+    target.setLevel(logging.INFO)
+
+    fm = RedactingFormatter(list(PII_FIELDS))
+    target.setFormatter(fm)
+
+    log.addHandler(target)
+    return log
+
+
+def main() -> None:
+    """ Main function to retrieve and log user data from the database. """
+    db = get_db()
+    pointer = db.cursor()
+    pointer.execute("SELECT * FROM users;")
+
+    headings = [field[0] for field in pointer.description]
+    log = get_logger()
+
+    for row in pointer:
+        info_answer = ''
+        for f, p in zip(row, headings):
+            info_answer += f'{p}={(f)}; '
+        log.info(info_answer)
+
+    pointer.close()
+    db.close()
 
 
 if __name__ == '__main__':

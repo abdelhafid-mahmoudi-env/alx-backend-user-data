4183f62206036fe4deca6a1a34956a19c11a2fbb
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-08-01 20:52:05 +0000

    test

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,4 +29,5 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+        return filter_datum(self.fields, self.REDACTION, message,
+                            self.SEPARATOR)

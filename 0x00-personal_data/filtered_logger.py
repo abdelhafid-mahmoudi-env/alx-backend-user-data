4ce028bfcbe9f4d8217fa31920404b0086a6cc3b
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-08-02 00:35:46 +0000

    test

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,8 +16,8 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     """Returns the log message obfuscated
 
     Args:
-        fields (List[str]): a list of strings representing all fields to obfuscate
-        redaction (str): a string representing by what the field will be obfuscated
+        fields (List[str]): a list of strings representing all fields.
+        redaction (str): a string representing by what the field.
         message (str): a string representing the log line
         separator (str): a string representing by which character is separating
                          all fields in the log line
